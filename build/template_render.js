10effd2c3cb1ea81de82b7931efcb1a01d1d7c09
Kate Preston <ktweeden@googlemail.com>
2018-02-15 14:38:39 +0000

    index, footer and header templating

--- build/template_render.js
+++ build/template_render.js
@@ -1,4 +1,5 @@
 const contentList = require("../templates/content_list.json")
+const navList = require("../templates/navigation.json")
 const nunjucks = require("nunjucks")
 const fs = require("fs")
 const path = require("path")
@@ -10,20 +11,20 @@ nunjucks.configure((path.join(__dirname, "../templates")), {
 function renderBlog() {
   for (let content of contentList) {
     const contentPath = `${content.url}.njk`
-    const fileContent = (nunjucks.render(contentPath))
+    const fileContent = (nunjucks.render(contentPath, {navList: navList}))
     const filePath = `./${content.url}.html`
     fs.writeFile(filePath, fileContent, (err) => {if (err) throw err})
   }
 }
 
 function renderIndex() {
-  const fileContent = (nunjucks.render("index.njk"))
+  const fileContent = (nunjucks.render("index.njk", {navList: navList, contentList: contentList}))
   const filePath = ("./index.html")
   fs.writeFile(filePath, fileContent, (err) => {if (err) throw err})
 }
 
 function renderAboutMe() {
-  const fileContent = (nunjucks.render("about_me.njk"))
+  const fileContent = (nunjucks.render("about_me.njk", {navList: navList}))
   const filePath = ("./about_me.html")
   fs.writeFile(filePath, fileContent, (err) => {if (err) throw err})
 }
